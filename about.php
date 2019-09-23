865d275897542f1de2973c66e8775b41f9950da8
nikunj0777 <52937048+nikunj0777@users.noreply.github.com>
2019-09-23 22:39:17 +1200

    Update about.php

--- about.php
+++ about.php
@@ -1,35 +1,35 @@
+<?php include('admin/lib_include/connection.inc.php'); ?>
 <!doctype html>
 <html class="no-js" lang="en">
 <head>
-	<meta charset="utf-8">
+   	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<title>Therapy</title>
-
-
-	<?php include "lib/top.inc.php" ?>
+	<meta name="description" content="">
+	<meta name="keywords" content="" />
+	<meta name="author" content="">
+<?php include "lib/top.inc.php" ?>
 </head>
-
 <body>
-	<?php include "lib/header.inc.php" ?>
+<?php include "lib/header.inc.php" ?>
 <!-- Showcase Section Starts here -->
-
-	<section id="sub-banner">
-		<img src="img/about-banner.jpg" alt="" />
-		
-		<div class="bredcrumb">
-			<div class="container">
-				<div class="row">	
-					<div class="col-sm-12">
-						<h3>About us</h3>
-						<ul>
-							<li><a href="<?php echo $basepath;?>">Home</a></li>
-							<li><a href="javascript:void(0);">About us</a></li>						
-						</ul>
-					</div>
+<section id="sub-banner">
+	<img src="img/about-banner.jpg" alt="" />
+	<div class="bredcrumb">
+		<div class="container">
+			<div class="row">	
+				<div class="col-sm-12">
+					<h3>About us</h3>
+					<ul>
+						<li><a href="<?php echo $basepath;?>">Home</a></li>
+						<li><a href="javascript:void(0);">About us</a></li>						
+					</ul>
 				</div>
 			</div>
 		</div>
+	</div>
 </section>
+
 
 <section id="about-section">
 	<div class="container">
@@ -43,13 +43,28 @@
 
 		<div class="row">	
 			<div class="col-sm-6">
-				<img src="img/sider-2.jpg" width="100%" height="315"  frameborder="0" allowfullscreen>
+			<img src="img/m.png" alt="" />
 			</div>
-
 			<div class="col-sm-6">
-			Let’s talk about what Body Therapy. Body is not a clinic.
-			we opened our doors with the mission to make a positive difference in the world, one guest at a time.
-			To do so, we pioneered an affordable luxury concept. Before Spavia, you could go to a cheaper-chain “spa” or splurge at a high-end resort to “get your spa on.” As a guest at Spavia, you receive a resort-like experience at an affordable price. You are welcomed to a space where you get to change into a luscious robe and relax in a retreat room before your service. This was an abstract concept before our inception.
+				A full-body massage is good for your heart as well. The vasodilation produced by massage increases your venous return which, in turn, increases blood flow and delivery of oxygen to all your organs. Your entire cardiovascular system relaxes and circulation throughout your body improves
+			
+			<p>
+				<h5><b>These are the 10 most common benefits reported from massage therapy </b></h5>
+				<br>
+				<ul>
+					<li>1. Reduce Stress</li>
+					<li>2. Improve Circulation</li>
+					<li>3. Reduce Pain</li>
+					<li>4. Eliminate Toxins</li>
+					<li>5. Improve Flexibility</li>
+					<li>6. Improve Sleep</li>
+					<li>7. Enhance Immunity</li>
+					<li>8. Reduce Fatigue</li>
+					<li>9. Alleviate Depression and Anxiety</li>
+					<li>10. Reduce post-surgery and post-injury swelling</li>
+					
+				</ul>
+			</p>	
 			</div>
 		</div>
 	</div>
@@ -60,76 +75,44 @@
 <section id="testimonials_section">
 	<div class="container">
 		<div class="row">	
-			<div class="col-sm-12">
+			<div class="col-sm-12" style="padding-top:50px; padding-bottom:50px;">
 				<h1>Client Testimonials</h1>
 				<div id="cl-testimonials" class="owl-carousel owl-theme owl-loaded">              
-
-						
-						
+				
+					<div class="owl-item active" style="width: 1140px; margin-right: 10px;">
+				<div class="item">
+					<div class="cl-speak">
+						<em><p>
+		<span style="font-family: Consolas, &quot;Lucida Console&quot;, monospace; white-space: pre-wrap;">
+		Teresa is by far the most amazing massage therapist. She has a personal interest in each and every person she works on.  When she comes to my gym to work on my members I know they are in the best of hands.  I wouldn't send them to anyone other than Bronwyn.  Her knowledge, caring and understanding of the human system and what makes it work and how to heal it is a gift and I consider myself fortunate to have her talents available to myself and my clients and members.</span></p>
+	</em>
+						<div class="authorInfo">
+							<img src="img/m.png" style="border-radius:100%;" >
+							<p><span>Mandeep Multani</span></p></div>
+					</div>
+				</div>
+			</div>
 			
-			<div class="owl-stage-outer"><div class="owl-stage" style="transform: translate3d(-2300px, 0px, 0px); transition: all 0s ease 0s; width: 6900px;"><div class="owl-item cloned" style="width: 1140px; margin-right: 10px;"><div class="item">
-				<div class="cl-speak">
-					<em>
-						<p>
-						<span style="font-family: Consolas, &quot;Lucida Console&quot;, monospace; white-space: pre-wrap;">Duis non condimentum nunc. Nunc quis turpis eu est tincidunt rutrum. Cras a purus quis sem tincidunt egestas vel id lacus. Nunc sed volutpat erat. Duis sollicitudin ac nisi eget rutrum. Nunc sagittis velit nec sapien blandit molestie.
-						</span>
-					</p>
-					</em>
-					<div class="authorInfo">
-						<img src="img/5646_cl_bg.png" alt="nick GRIFFITH">
-						<p><span>Mandeep Multani</span></p></div>
-				</div>
-			</div></div><div class="owl-item active" style="width: 1140px; margin-right: 10px;"><div class="item">
-				<div class="cl-speak">
-					<em>
-						<p>
-							<span style="font-family: Consolas, &quot;Lucida Console&quot;, monospace; white-space: pre-wrap;">Duis non condimentum nunc. Nunc quis turpis eu est tincidunt rutrum. Cras a purus quis sem tincidunt egestas vel id lacus. Nunc sed volutpat erat. Duis sollicitudin ac nisi eget rutrum.</span></p>
-					</em>
-					
-					<div class="authorInfo">
-						<img src="img/29606_cl_bg.png" alt="MELANI GRIFFITH ">
-					
-						<p><span>Nick Patel </span></p></div>
-				</div>
+			<div class="owl-item active" style="width: 1140px; margin-right: 10px;">
+				<div class="item">
+					<div class="cl-speak">
+						<em><p>
+		<span style="font-family: Consolas, &quot;Lucida Console&quot;, monospace; white-space: pre-wrap;">
+		Camila is by far the most amazing massage therapist. She has a personal interest in each and every person she works on.  When she comes to my gym to work on my members I know they are in the best of hands.  I wouldn't send them to anyone other than Bronwyn.  Her knowledge, caring and understanding of the human system and what makes it work and how to heal it is a gift and I consider myself fortunate to have her talents available to myself and my clients and members.</span></p>
+	</em>
+						<div class="authorInfo">
+							<img src="img/m.png" style="border-radius:100%;" >
+						<p><span>Nikunj Patel</span></p></div>
+					  </div>
+				   </div>
+			     </div>
 			</div>
-		</div>
-	<div class="owl-item active" style="width: 1140px; margin-right: 10px;"><div class="item">
-	
-			<div class="cl-speak">
-					<em><p>
-	
-					<span style="font-family: Consolas, &quot;Lucida Console&quot;, monospace; white-space: pre-wrap;">Duis non condimentum nunc. Nunc quis turpis eu est tincidunt rutrum. Cras a purus quis sem tincidunt egestas vel id lacus. Nunc sed volutpat erat. Duis sollicitudin ac nisi eget rutrum. Nunc sagittis velit nec sapien blandit molestie.</span></p>
-
-					</em>
-					<div class="authorInfo">
-						<img src="img/29606_cl_bg.png" alt="MELANI GRIFFITH ">
-						<p><span>Shankar Pillai </span></p></div>
-					</div>
-				</div>
+		 </div>
+			<div class="owl-controls"><div class="owl-nav"><div class="owl-prev" style="display: none;">prev</div><div class="owl-next" style="display: none;">next</div></div><div class="owl-dots" style=""><div class="owl-dot active"><span></span></div><div class="owl-dot"><span></span></div></div></div></div>
 			</div>
 		</div>
 	</div>
-	
-	<div class="owl-controls">
-		<div class="owl-nav">
-			<div class="owl-prev" style="display: none;">prev</div>
-			<div class="owl-next" style="display: none;">next</div>
-		</div>
-		
-					<div class="owl-dots" style="">
-						<div class="owl-dot active"><span></span>
-					</div>
-				 		<div class="owl-dot"><span></span>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
 </section>
-
-
 <?php include "lib/footer.inc.php" ?>
 
 <script type="text/javascript" src="js/wowslider.js"></script>
